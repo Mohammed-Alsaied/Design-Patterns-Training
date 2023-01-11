5efcb7ed15c152062810ad3c31b17cba704dfa1c
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-11 17:16:16 +0200

    Factory Method

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -7,7 +7,7 @@ var factories = new List<DiscountFactory>
     new CodeDiscountFactory(Guid.NewGuid()),
     new CountryDiscountFactory("BE"),
 };
-
+//
 foreach (var factory in factories)
 {
     var discountServices = factory.CreateDiscountService();
